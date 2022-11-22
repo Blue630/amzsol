48c63263aed2bf6d46d733162ae3ae69c19f65a0
GlobalStar1115 <bigstarcoolmanager@gmail.com>
2022-11-22 03:05:21 -0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import { Router, Switch, Route } from 'react-router-dom'
+import history from './utils/history'
+import './App.css';
+
+import Calendarview from './pages/Calendarview'
+import CreateASINcampaign from './pages/CreateASINcampaign'
+import CreateKWcampaign from './pages/CreateKWcampaign'
+import Transactionwithtabs from './pages/Transactionwithtabs'
+import StartPage from './pages/StartPage'
+import CompanySettings from './pages/CompanySettings'
+import Details from './pages/Details'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css'
+function App() {
+  return (
+    <Router history={history} >
+      <Switch>
+        <Route
+          path="/startpage"
+          component={StartPage}
+        />
+        <Route
+          path="/companysettings"
+          component={CompanySettings}
+        />
+        <Route
+          path="/Details"
+          component={Details}
+        />
+        <Route
+          path="/createASINcampaign"
+          component={CreateASINcampaign}
+        />
+        <Route
+          path="/createKWcampaign"
+          component={CreateKWcampaign}
+        />
+        <Route path="/transactionwithtabs" component={Transactionwithtabs} />
+        <Route
+          path="/"
+          component={Calendarview}
+        />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,41 @@
+import {Container, Navbar, NavDropdown, Nav} from 'react-bootstrap';
+import {ReactComponent as UserSVG} from './../../user.svg';
+import './index.css';
+
+function Header(props) {
+  return (
+    <>
+      {[false].map((expand, index) => (
+        <Navbar key={index} collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container>
+            <Navbar.Brand href="#home"><img className='logo-img' src="./../../logo.png" /></Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            {
+              props.showOthers && 
+              <Navbar.Collapse id="responsive-navbar-nav">
+                  <Nav className="me-auto">
+                    <Nav.Link href="campaign.html">Dashboard</Nav.Link>
+                    <Nav.Link href="rules.html">File Settings</Nav.Link>
+                  </Nav>
+                  <Nav>
+                    <UserSVG />
+                    <NavDropdown title="uchiyama@j-wire.info-San" id="collasible-nav-dropdown">
+                        <NavDropdown.Item href="/subscriptions/new">Plan Management</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="/profile">Profile</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="/payments">Payment Methods</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="/users/sign_out">Logout</NavDropdown.Item>
+                    </NavDropdown>
+                  </Nav>
+              </Navbar.Collapse>
+            }
+            </Container>
+        </Navbar>
+      ))}
+    </>
+  );
+}
+
+export default Header;
